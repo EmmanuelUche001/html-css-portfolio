747c6ca08d25d44b1f033df216fa04a0916e93fa
EmmanuelUche001 <okechukwuemmanuel79@gmail.com>
2024-10-15 09:43:09 -0500

    Finished Portfolio Tutorial

---
+++ Script.js
@@ -0,0 +1,6 @@
+function togglemenu () {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hanburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
